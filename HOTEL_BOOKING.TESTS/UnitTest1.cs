4e7d50c59de30bec245ff2b5a324371e29e7aced
samba439 <k.ksamba@outlook.com>
2018-07-20 23:45:36 +0530

    adding solution to git

---
+++ HOTEL_BOOKING.TESTS/UnitTest1.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace HOTEL_BOOKING.TESTS
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+}
